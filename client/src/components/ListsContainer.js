9af671e3f995cb84dec28780c47de06667b09d96
Marcos Scorzoni <mlessa@gmail.com>
2018-09-17 21:41:36 +0100

    add a list component

---
+++ client/src/components/ListsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListsContainer extends Component {
+  render() {
+    return (
+      <div className="Lists-container">
+        Lists
+      </div>
+      )
+  }
+}
